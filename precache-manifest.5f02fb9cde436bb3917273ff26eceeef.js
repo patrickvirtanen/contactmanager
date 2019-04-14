15730229cbb8f1f6b4c28c4c63586cc20d3a28b1
patrick <patrick.virtanen@gmail.com>
2019-04-14 19:36:15 +0200

    Updates

---
+++ precache-manifest.5f02fb9cde436bb3917273ff26eceeef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4bf40f27ed8dbd6f9da",
+    "url": "/contactmanager/static/js/runtime~main.e4bf40f2.js"
+  },
+  {
+    "revision": "97f38a00eb08382a71bb",
+    "url": "/contactmanager/static/js/main.97f38a00.chunk.js"
+  },
+  {
+    "revision": "692cc46acb58ef521d33",
+    "url": "/contactmanager/static/js/2.692cc46a.chunk.js"
+  },
+  {
+    "revision": "97f38a00eb08382a71bb",
+    "url": "/contactmanager/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "692cc46acb58ef521d33",
+    "url": "/contactmanager/static/css/2.596468fb.chunk.css"
+  },
+  {
+    "revision": "fce190f54ecf29c7fb5b7ecad086da85",
+    "url": "/contactmanager/index.html"
+  }
+];
